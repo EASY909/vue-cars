52f1ee2ed1d8dd679d4371adbad4b22a98a99aab
easy909 <1730127479@qq.com>
2020-12-20 21:26:15 +0800

    card

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -9,8 +9,36 @@
         </h4>
         <p class="attr">新能源汽车 5座</p>
       </header>
-      <div class="cars-content"></div>
-      <footer></footer>
+      <div class="cars-content">
+        <div class="info">
+          <div>
+            <h4 class="cars-number">皖 AE86</h4>
+            <div>
+              <ul class="cars-electric active-li-2">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <p class="distance">
+                <sub>约</sub>
+                <strong>600</strong>
+                <sub>KM</sub>
+              </p>
+            </div>
+          </div>
+        </div>
+        <img src="../../assets/images/pic001.jpg" alt="" />
+      </div>
+      <footer>
+        <a href="javascript: void(0);" class="parking-link">停车场</a>
+      </footer>
     </section>
     <span @click="user"></span>
   </div>
@@ -65,6 +93,7 @@ export default {
   height: 257px;
   margin: auto;
   padding: 30px;
+  padding-top: 10px;
   background-color: #fff;
   border-radius: 24px;
   @include webkit(box-shadow, 0 0 18px 0 rgba(0, 0, 0, 0.2));
@@ -92,5 +121,80 @@ export default {
   text-align: right;
   opacity: 0.5;
   line-height: 60px;
+}
+.cars-content {
+  position: relative;
+  margin: 5px 0 7px;
+  @extend %clearfix;
+  img {
+    float: right;
+    height: 120px;
+  }
+  .info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+  }
+  .cars-number {
+    font-size: 18px;
+  }
+  .cars-electric {
+    padding-top: 10px;
+    display: inline-block;
+    li {
+      display: inline-block;
+      width: 7px;
+      height: 24px;
+      margin-right: 3px;
+      background-color: #e1e1e1;
+      border-radius: 100px;
+    }
+    @for $i from 1 through 10 {
+      &.active-li-#{$i} {
+        li:nth-child(-n + #{$i}) {
+          @include webkitA(background, linear-gradient(#17a8fa, #108dd9));
+        }
+      }
+    }
+  }
+}
+.distance {
+  margin-left: 5px;
+  display: inline-block;
+  strong {
+    margin: 0 3px;
+    font-size: 22px;
+  }
+  sub {
+    position: relative;
+    top: -2px;
+  }
+  sub:first-of-type {
+    opacity: 0.6;
+  }
+}
+.parking-link {
+  position: relative;
+  display: block;
+  border-top: 1px solid #e1e1e1;
+  color: $color-main;
+  opacity: 0.8;
+  line-height: 53px;
+  &:after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 50%;
+    width: 8px;
+    height: 8px;
+    margin-top: -8px;
+    border-top: 1px solid #d6d7d9;
+    border-right: 1px solid #d6d7d9;
+    @include webkit(transform, rotate(45deg));
+  }
 }
 </style>
